7a7f3b88c7ade73bb5b9e35aa21f577bb9429546
Hamid Azim <hamidazim321@gmail.com>
2023-08-14 03:01:57 -0700

    Add function to add and delete todo items

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,7 +1,8 @@
 import InputTodo from "components/InputTodo";
 import TodosList from "components/TodoList";
+import { useState } from "react";
 const TodosLogic = () => {
-    const todos = [
+    const [todos, setTodos] = useState([
       {
         id: 1,
         title: 'Setup development environment',
@@ -17,11 +18,43 @@ const TodosLogic = () => {
         title: 'Deploy to live server',
         completed: false,
       },
-    ];
+    ]);
+
+    const addTodoItem = (title) => {
+      const newTodo = {
+        id: 4,
+        title: title,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+    };
+    
+
+    const handleChange = (id) => {
+      setTodos((prevState) =>
+        prevState.map((todo) => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              completed: !todo.completed,
+            };
+          }
+          return todo;
+        })
+      );
+    };
+
+    const delTodo = (id) => {
+      setTodos([
+        ...todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ]);
+    };    
     return (
 			<div>
-				<InputTodo />
-				<TodosList todosProps={todos} />
+				<InputTodo addTodoItem={addTodoItem}/>
+				<TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo}/>
 			</div>
 		);
   };
