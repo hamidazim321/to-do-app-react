408c5842a5e4757dd5d3b59a714de98bb808370f
Hamid Azim <hamidazim321@gmail.com>
2023-08-14 04:55:20 -0700

    Add styles and save todos on local storage

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 const InputTodo = ({addTodoItem}) => {
   const [title, setTitle] = useState('');
+  const [message, setMessage] = useState(null);
 
   const handleChange = (e) => {
     setTitle(e.target.value);
@@ -8,20 +9,29 @@ const InputTodo = ({addTodoItem}) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodoItem(title);
-    setTitle('');
+    if (title.trim()) {
+      addTodoItem(title);
+      setTitle('');
+      setMessage(null)
+    } else {
+      setMessage('please add an item')
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add Todo..."
-        value={title}
-        onChange={handleChange}
-      />
-      <button>Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}  className="form-container">
+        <input
+          className="input-text"
+          type="text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={handleChange}
+        />
+        <button className="input-submit">Submit</button>
+      </form>
+      {message && <span className="submit-warning">{message}</span>}
+    </>
   );
 };
 export default InputTodo;
--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,62 +1,74 @@
 import InputTodo from "components/InputTodo";
 import TodosList from "components/TodoList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 const TodosLogic = () => {
-    const [todos, setTodos] = useState([
-      {
-        id: 1,
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: 2,
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: 3,
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ]);
+  
+  const initialTodos = () => {
+    return JSON.parse(localStorage.getItem('todos')) || []
+  }
 
-    const addTodoItem = (title) => {
-      const newTodo = {
-        id: 4,
-        title: title,
-        completed: false,
-      };
-      setTodos([...todos, newTodo]);
+  const [todos, setTodos] = useState(initialTodos());
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+    console.log(initialTodos())
+  }, [todos]);
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
     };
-    
-
-    const handleChange = (id) => {
-      setTodos((prevState) =>
-        prevState.map((todo) => {
-          if (todo.id === id) {
-            return {
-              ...todo,
-              completed: !todo.completed,
-            };
-          }
-          return todo;
-        })
-      );
-    };
-
-    const delTodo = (id) => {
-      setTodos([
-        ...todos.filter((todo) => {
-          return todo.id !== id;
-        }),
-      ]);
-    };    
-    return (
-			<div>
-				<InputTodo addTodoItem={addTodoItem}/>
-				<TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo}/>
-			</div>
-		);
+    setTodos([...todos, newTodo]);
   };
+
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };  
+  
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <InputTodo addTodoItem={addTodoItem}/>
+      <TodosList 
+        todosProps={todos} 
+        handleChange={handleChange} 
+        delTodo={delTodo}
+        setUpdate={setUpdate}
+        />
+    </div>
+  );
+};
 export default TodosLogic;
   
